e71a2339621567173a570668b258e57f12c8e4bd
purpleloop <38167219+purpleloop@users.noreply.github.com>
2024-12-28 01:20:02 +0100

    Upgrade * Java 21 * log4j.version 2.24.3 * commons-logging.version 1.3.4 * commons-lang.version 3.17.0 * junit.jupiter.version 5.11.4 + fix modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,10 @@ module io.guithub.purpleloop.svgtools {
     exports io.github.purpleloop.tools.svg.gui;
     exports io.github.purpleloop.tools.svg.tools;
     
+    requires transitive java.desktop;
     requires io.github.purpleloop.commons;
     requires io.github.purpleloop.commons.swing;
-    requires transitive java.desktop;
-	requires transitive commons.logging;
+	requires transitive org.apache.commons.logging;
     requires org.apache.logging.log4j;
     requires org.apache.commons.lang3;
     requires xmlwriter;
